2f0e5448e0fc1b2309f54a0c94c57762a50eadaa
Michelle Bang <bangm@oregonstate.edu>
2023-01-12 01:34:49 -0800

    hello world!

---
+++ assignment0/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+//Just a simple hello world program!
+int main(int argc, char **argv) {
+    cout << "Hello world!" << endl;
+
+    return 0;
+}
