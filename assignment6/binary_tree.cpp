dcc8998525dce9e39e55d53956b88f012f7b5477
Michelle Bang <bangm@oregonstate.edu>
2023-02-26 18:15:16 -0800

    working...

--- assignment6/binary_tree.cpp
+++ assignment6/binary_tree.cpp
@@ -69,10 +69,37 @@ Binary_node *most_right(Binary_node *cur_root){
     
 }
 
-
-Binary_node *remove_node(Binary_node *cur_root, int num){
+Binary_node *node_before(Binary_node *cur_root, Binary_node *interest){
     Binary_node *current = cur_root;
     Binary_node *future = current;
+
+    while(future->value != interest->value){
+        if(interest->value < current->value){
+            current = future;
+            future = current->left;
+        }else{
+            current = future;
+            future = current->right;
+        }
+    }
+
+    return current;
+    
+}
+
+
+Binary_node *remove_node(Binary_node *cur_root, int num, Binary_node *parent_node){
+    Binary_node *current = cur_root;
+    Binary_node *future = current;
+    Binary_node *parent = parent_node;
+
+    if(current->value > num){
+        current = future;
+        future = current->left;
+    } else if(current->value < num){
+        current = future;
+        future = current->right;
+    }
 
     if(current->value == num){
         if(current->left != nullptr){
@@ -81,27 +108,55 @@ Binary_node *remove_node(Binary_node *cur_root, int num){
             replacement_node->left = current->left;
             delete current;
             replacement_node_before->left = nullptr;
+            return replacement_node;
         } else{
+            Binary_node *before_node = node_before(parent_node, current);
+            before_node->left = nullptr;
             delete current;
+            return before_node;
         }
-        return current;
-    } else if(current->value > num){
-        current = future;
-        future = current->left;
-        remove_node(future, num);
-    } else{
-        current = future;
-        future = current->right;
-        remove_node(future, num);
     }
 
-    //idea: start at root... compare values until get to node with value inputted... figure out way to choose what node to replace value... (ask question)... make new tree w/o value.. delete value..
+    return remove_node(future, num, parent_node);
+    
+    // Old code
+    // if(current->value == num){
+    //     if(current->left != nullptr){
+    //         Binary_node *replacement_node_before = most_right(current->left);
+    //         Binary_node *replacement_node = replacement_node_before->left;
+    //         replacement_node->left = current->left;
+    //         delete current;
+    //         replacement_node_before->left = nullptr;
+    //         return replacement_node;
+    //     } else{
+    //         Binary_node *before_node = node_before(parent_node, current);
+    //         before_node->left = nullptr;
+    //         delete current;
+    //         return before_node;
+    //     }
+    // } else if(current->value > num){
+    //     current = future;
+    //     future = current->left;
+    // } else{
+    //     current = future;
+    //     future = current->right;
+    // }
+
+    //pre-code notes:
+    /*idea: start at root... 
+    compare values until get to node with value inputted... 
+    figure out way to choose what node to replace value... 
+    (ask question)... make new tree w/o value.. delete value..*/
     //log(n)
     //either biggest left or smallest right
     //compare with node that you remove
 }
 
 int main(){
+    //Notes:
+    //& address of object
+    //* pointer of object
+
     Binary_node root{42, nullptr, nullptr};
     Binary_node left{21, nullptr, nullptr};
     Binary_node right{63, nullptr, nullptr};
@@ -110,8 +165,12 @@ int main(){
     root.right = &right;
 
     insert_node(&root, 5);
-    //& address of object
-    //* pointer of object
+
+    in_order_traversal(&root);
+
+    remove_node(&root, 5, &root);
+
+    in_order_traversal(&root);
 
     return 0; 
 }
